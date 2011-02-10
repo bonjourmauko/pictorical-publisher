3b119c0ce6a8239b7ed8655a9d00fe080696c4f0
maukoquiroga <mauko@pictorical.com>
2011-02-10 20:13:11 -0300

    Creating the "publisher.pictorical.com" repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PublisherPictoricalCom::Application.config.secret_token = '11389feb8d721446398331671a4d9ec8a53fbc91243ce9668994f05db42ad91cc4ff2dce6af1adf2945a248ac625ca039efc1b844de6a60cf17371d27aa29c81'
