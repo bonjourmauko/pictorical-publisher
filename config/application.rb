f8e9562eb3aef00343c14c8a1e3847a38bcbdfd5
maukoquiroga <mauko@pictorical.com>
2011-02-11 15:30:36 -0300

    Victor! Skeleton Ready

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module PublisherPictoricalCom
   
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w()
+    config.action_view.javascript_expansions[:defaults] = %w(jquery.min rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
