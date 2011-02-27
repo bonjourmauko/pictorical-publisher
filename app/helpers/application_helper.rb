2fa9ab68e7133b85e25ddc697091c728f4068cf0
victor <victor@pictorical.com>
2011-02-26 21:15:49 -0800

    texts#show ready

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def title(title)
+    content_for :title, title
+  end
+  
+  def stylesheets(*args)
+    content_for :head, stylesheet_link_tag(*args)
+  end
+  
+  def javascripts(*args)
+    content_for :head, javascript_include_tag(*args)
+  end
 end
---
+++ public/javascripts/text.js
@@ -0,0 +1,40 @@
+function pad(number, length) {
+
+    var str = '' + number;
+    while (str.length < length) {
+        str = '0' + str;
+    }
+
+    return str;
+
+}
+
+
+$(document).ready(function() {
+  
+  var wordcount = 0;
+  
+
+  $('p:first').before('<div class="parnum">CAP</div>');
+  $('p:last').after('<h3>The End</h3>');
+
+  $('p,h2-h2:first').each(function(index) {
+
+    var positionemail = pad(index+1,3);
+    $(this).after('<div class="parnum">'+pad(index+1,3)+'</div>');
+
+    var string = $(this).text();
+    var count = string.split(" "); 
+    wordcount = wordcount + count.length;
+
+    });
+
+    wordcount = wordcount/1.2; // correction for number of word 
+
+    var number_of_illustrations = wordcount/300;
+    var number_of_illustrations_top = Math.round(number_of_illustrations*1.2);
+    var number_of_illustrations_bottom = Math.round(Math.min(wordcount/300,20+(wordcount-6000)/600,30+(wordcount-12000)/1200)*0.9);
+
+    $('#number_of_illustrations').html( number_of_illustrations_bottom + "-" + number_of_illustrations_top );
+
+});
