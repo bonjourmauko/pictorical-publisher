08aa28b8cfd72cfd9e0d6f070fcd2f41dc9d1828
victor <victor@pictorical.com>
2011-03-05 17:09:37 -0800

    prettifying step 1, w/cancan

--- app/helpers/texts_helper.rb
+++ app/helpers/texts_helper.rb
@@ -1,12 +1,12 @@
 module TextsHelper
   def illustrate_or_change
     if @active_book.nil?
-      link_to "Illustrate text", :controller => "books", :action => "new", :text_id => @text[:id]
+      link_to "Illustrate text", :controller => "books", :action => "new", :text_id => @text[:id], :class => "illustrate-this-link"
     else
       if @active_book.text[:id] == @text[:id]
-        link_to "You are illustrating this book", :controller => "books", :action => "edit", :id => @active_book.id
+        link_to "You are illustrating this book", :controller => "books", :action => "edit", :id => @active_book.id, :class => "illustrate-current-link"
       else
-        link_to "Change to this story", :controller => "books", :action => "change", :text_id => @text[:id]
+        link_to "Change to this story", :controller => "books", :action => "change", :text_id => @text[:id], :class => "illustrate-change-link"
       end
     end
   end
