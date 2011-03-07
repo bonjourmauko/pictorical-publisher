6d169328c80409f5f613896b37bd2db71837db04
maukoquiroga <mauko@pictorical.com>
2011-03-07 02:11:51 -0300

    remove validation of name in Author

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,8 +2,7 @@ class Author < ActiveRecord::Base
   has_many  :texts
   scope :sorted, order('last_name ASC')
   
-  validates_presence_of :name,
-                        :last_name
+  validates_presence_of :last_name
   
   def name_with_initial
     "#{name.first}. #{last_name}"
