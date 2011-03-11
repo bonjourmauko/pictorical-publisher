2fa81e5ef034e0d90cb88b1bf08be57997cc7700
maukoquiroga <mauko@pictorical.com>
2011-03-11 01:39:10 -0300

    erm

--- db/migrate/20110311042017_set_availability_to_true.rb
+++ db/migrate/20110311042017_set_availability_to_true.rb
@@ -1,6 +1,6 @@
 class SetAvailabilityToTrue < ActiveRecord::Migration
   def self.up
-    Text.all { |text| text[:availability] = true if !text.first.nil? }
+    Text.all.each { |text| text[:availability] = true if !text.first.nil? }
   end
 
   def self.down
