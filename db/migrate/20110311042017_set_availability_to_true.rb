0c1fb714152fcdc43749fe5daf31d4858a44e379
maukoquiroga <mauko@pictorical.com>
2011-03-11 01:54:46 -0300

    :O ya pos migracion

--- db/migrate/20110311042017_set_availability_to_true.rb
+++ db/migrate/20110311042017_set_availability_to_true.rb
@@ -1,6 +1,6 @@
 class SetAvailabilityToTrue < ActiveRecord::Migration
   def self.up
-    Text.all.each { |text| text[:availability] = true }
+    Text.all.each { |text| text[:availability] = true; text.save }
   end
 
   def self.down
