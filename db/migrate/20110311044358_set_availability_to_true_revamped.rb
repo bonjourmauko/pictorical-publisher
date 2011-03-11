624eb9f5875589030859a06786a3753f1376970c
maukoquiroga <mauko@pictorical.com>
2011-03-11 01:46:48 -0300

    spelling error

--- db/migrate/20110311044358_set_availability_to_true_revamped.rb
+++ db/migrate/20110311044358_set_availability_to_true_revamped.rb
@@ -1,6 +1,6 @@
 class SetAvailabilityToTrueRevamped < ActiveRecord::Migration
   def self.up
-    Text.all.each { |text| text[:availability] = true if !text.first.nil?; text.save }
+    Text.all.each { |text| text[:availability] = true; text.save }
   end
 
   def self.down
