ff9aeb64aeab17c1ead4710057e7d72d472f9e96
maukoquiroga <mauko@pictorical.com>
2011-03-11 01:50:24 -0300

    ya po migracion funciona

--- db/migrate/20110311042017_set_availability_to_true.rb
+++ db/migrate/20110311042017_set_availability_to_true.rb
@@ -1,6 +1,6 @@
 class SetAvailabilityToTrue < ActiveRecord::Migration
   def self.up
-    Text.all.each { |text| text[:availability] = true if !text.first.nil? }
+    Text.all.each { |text| text[:availability] = true }
   end
 
   def self.down
--- db/migrate/20110311044358_set_availability_to_true_revamped.rb
+++
@@ -1,8 +0,0 @@
-class SetAvailabilityToTrueRevamped < ActiveRecord::Migration
-  def self.up
-    Text.all.each { |text| text[:availability] = true; text.save }
-  end
-
-  def self.down
-  end
-end
