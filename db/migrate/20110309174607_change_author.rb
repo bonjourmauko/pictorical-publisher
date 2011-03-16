c42435219773e42ff813f43dac6822e5f75a0863
maukoquiroga <mauko@pictorical.com>
2011-03-15 20:21:05 -0400

    corrected some queries

--- db/migrate/20110309174607_change_author.rb
+++ db/migrate/20110309174607_change_author.rb
@@ -1,7 +1,7 @@
 class ChangeAuthor < ActiveRecord::Migration
   def self.up
     say_with_time("Changing author to text") do
-      Text.where(:id => 370) { |text| text[:author_id] = 20 if !text.first.nil? }
+      Text.where(:id => 370) { |text| text[:author_id] = 20 if text? }
     end
   end
 
