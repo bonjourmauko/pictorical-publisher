1e0ba1b94290ae3495447d66ad067f62bbb36d66
maukoquiroga <mauko@pictorical.com>
2011-04-18 22:30:44 -0400

    asdf

---
+++ db/migrate/20110419015634_set_published_for_andersen_stories.rb
@@ -0,0 +1,19 @@
+class SetPublishedForAndersenStories < ActiveRecord::Migration
+  def self.up
+    say_with_time("Asign published time to Andersen's and Grimm's texts") do
+      Text.where(:author_id => 4).each do |text|
+        text[:published] = 0
+        text.save
+      end
+
+      Text.where(:author_id => 1).each do |text|
+        text[:published] = 0
+        text.save
+      end
+      
+    end
+  end
+
+  def self.down
+  end
+end
