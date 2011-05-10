5a2998a869502d00a99d1d547c4bf13873823d05
maukoquiroga <mauko@pictorical.com>
2011-05-10 19:31:51 -0400

    asdf

--- lib/content_parser.rb
+++ lib/content_parser.rb
@@ -1,62 +1,94 @@
 module ContentParser
+  
   def epubeize(book)
-    content = Nokogiri::HTML book.content
-    content.encoding = 'utf-8'
-
-    position = 1
-
-    content.css("p").each do |p|
-      if p['class'] == "first"
-        i = book.illustrations.active.where(:tipe => "cap").first
-
+    @content        = create_nokogiri_object(book.content, "utf-8")    
+    caps            = get_illustrations(book, "cap")
+    inlines         = get_illustrations(book, "inline")
+    cap_position    = 1
+    inline_position = 1
+    
+    @content.css("p").each do |node|
+      case node
+      when node[:class] == "first"
         div = Nokogiri::XML::Node.new "div", content
-        div['class'] = "initial7"
-        p.add_previous_sibling div
-
-        img = Nokogiri::XML::Node.new "img", content
-        img['src'] = "images/#{i[:tipe]}_#{i[:position]}_#{i[:image_original_id]}.#{i[:image_file_extension]}"
-        img['alt'] = "illustration"
-        img.parent = div
-
-        p.content = p.content[1..-1]
+        div[:class] = "initial7"
+        node.add_previous_sibling div      
+        add_img(div, caps.where(:position => cap_position))
+        node.content = node.content[1..-1]
+        cap_position += 1        
       else
-        book.illustrations.active.each do |i|
-          if i[:position] == position and i[:tipe] == "inline"
-            _p = Nokogiri::XML::Node.new "p", content
-            p.add_previous_sibling _p
-
-            img = Nokogiri::XML::Node.new "img", content
-            img['src'] = "images/#{i[:tipe]}_#{i[:position]}_#{i[:image_original_id]}.#{i[:image_file_extension]}"
-            img['alt'] = 'illustration'
-            img.parent = _p
-          elsif i[:position] == position + 1 and i[:tipe] == "inline" and content.search("p").last == p
-            _p = Nokogiri::XML::Node.new "p", content
-            p.add_next_sibling _p
-
-            img = Nokogiri::XML::Node.new "img", content
-            img['src'] = "images/#{i[:tipe]}_#{i[:position]}_#{i[:image_original_id]}.#{i[:image_file_extension]}"
-            img['alt'] = 'illustration'
-            img.parent = _p
-          end
+        inlines.each do |illustration|
+          offset = position_offset(node, illustration, inline_position)
+          add_sibling(node, offset, "p") if offset
         end
-      position += 1
+        inline_position += 1
       end
     end
 
-    content.css("blockquote").each do |blockquote|
-      p = Nokogiri::XML::Node.new "p", content
-      blockquote.children.each do |child|
-        child.parent = p
-      end
-      blockquote.add_child p
+    @content.css("blockquote").each do |node|
+      add_child(node, "p")
     end
     
-    nodes = content.css "blockquote"
-    wrapper = nodes.wrap("<div class='block'></div>")
+    wrap_content("blockquote", "div", "block")
     
-    content = content.to_html
-    content.gsub!(/<!.*?>/, '').gsub!(/<.*?html>/, '').gsub!(/<.*?body>/, '').gsub!('"illustration">', '"illustration" />')
-    content.gsub!("<br>", "<br />") if content =~ /<br.*?>/
+    html = @content.to_html
+    html = correct_markup(html)
+    html
+  end
+  
+  def create_nokogiri_object(html, encoding)
+    content = Nokogiri::HTML html
+    content.encoding = encoding
     content
   end
+  
+  def get_illustrations(book, tipe)
+    illustrations = book.illustrations.active.where(:tipe => tipe).order('position ASC')
+    illustrations
+  end
+  
+  def add_img(parent, illustration)
+      img = Nokogiri::XML::Node.new "img", @content
+      img[:src] = "images/#{illustration[:tipe]}_#{illustration[:position]}_#{illustration[:image_original_id]}.#{illustration[:image_file_extension]}"
+      img[:alt] = "illustration"
+      img.parent = parent
+  end
+  
+  def add_sibling(node, where, sibling)
+    _node = eval "Nokogiri::XML::Node.new '#{sibling}', @content"
+    node.send "add_#{where}_sibling", _node
+  end
+  
+  def add_child(node, child)
+    _child} = Nokogiri::XML::Node.new '#{child}', @content
+          #{node}.children.each do |child|
+            child.parent = #{child}
+          end
+          #{node}.add_child #{p}"
+  end
+  
+  def position_offset(node, i, position)
+    if i[:position] == position
+      "previous"
+    elsif i[:position] == position + 1 and @content.css("p").last == node
+      "next"
+    else
+      false
+    end
+  end
+  
+  def wrap_content(children, parent, parent_class)
+    nodes = @content.css "#{children}"
+    wrapper = nodes.wrap("<#{parent} class='#{parent_class}'></#{parent}>")
+  end
+  
+  def correct_markup(html)
+    html.gsub!(/<!.*?>/, '')
+    html.gsub!(/<.*?html>/, '')
+    html.gsub!(/<.*?body>/, '')
+    html.gsub!(/\"illustration\">/, '"illustration" />')
+    html.gsub!(/<br.*?>/, '<br />') if html =~ /<br.*?>/
+    html
+  end
+  
 end
