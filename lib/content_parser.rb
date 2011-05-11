216a768a28feb07199c2e4e70e04d585cf01a73a
maukoquiroga <mauko@pictorical.com>
2011-05-11 17:23:15 -0400

    epub 2.0

--- lib/content_parser.rb
+++ lib/content_parser.rb
@@ -2,29 +2,28 @@ module ContentParser
   
   def epubeize(book)
     @content        = create_nokogiri_object(book.content, "utf-8")    
-    caps            = get_illustrations(book, "cap")
-    inlines         = get_illustrations(book, "inline")
-    cap_position    = 1
-    inline_position = 1
+    @caps            = get_illustrations(book, "cap")
+    @inlines         = get_illustrations(book, "inline")
+    @cap_position    = 1
+    @inline_position = 1
     
     @content.css("p").each do |node|
-      case node
-      when node[:class] == "first"
-        div = Nokogiri::XML::Node.new "div", content
-        div[:class] = "initial7"
-        node.add_previous_sibling div      
-        add_img(div, caps.where(:position => cap_position))
+      if node['class'] == "first"      
+        add_img(@caps.where(:position => @cap_position).first, add_sibling(node, "previous", "div", "initial7")) 
         node.content = node.content[1..-1]
-        cap_position += 1        
+        @cap_position += 1        
       else
-        inlines.each do |illustration|
-          offset = position_offset(node, illustration, inline_position)
-          add_sibling(node, offset, "p") if offset
+        @inlines.each do |illustration|
+          offset = position_offset(node, illustration)
+          if offset
+            parent = add_sibling(node, offset, "p", "#{illustration[:position]}")          
+            add_img(illustration, parent)
+          end
         end
-        inline_position += 1
+        @inline_position += 1
       end
     end
-
+    
     @content.css("blockquote").each do |node|
       add_child(node, "p")
     end
@@ -47,21 +46,18 @@ module ContentParser
     illustrations
   end
   
-  def add_img(parent, illustration)
-      img = Nokogiri::XML::Node.new "img", @content
-      img[:src] = "images/#{illustration[:tipe]}_#{illustration[:position]}_#{illustration[:image_original_id]}.#{illustration[:image_file_extension]}"
-      img[:alt] = "illustration"
-      img.parent = parent
+  def add_img(illustration, parent)   
+    img = Nokogiri::XML::Node.new "img", @content
+    img['src'] = "images/#{illustration[:tipe]}_#{illustration[:position]}_#{illustration[:image_original_id]}.#{illustration[:image_file_extension]}"
+    img['alt'] = "illustration"
+    img.parent = parent
   end
   
-  def add_sibling(node, where, tag, params)
+  def add_sibling(node, where, tag, klass)
     _node = eval "Nokogiri::XML::Node.new '#{tag}', @content"
-    
-    params.each do |key, value|
-      eval "_node['#{key}'] = '#{value}'"
-    end
-    
+    _node['class'] = klass
     node.send "add_#{where}_sibling", _node
+    return _node
   end
   
   def add_child(node, child)
@@ -72,10 +68,11 @@ module ContentParser
     node.send "add_child", _node
   end
   
-  def position_offset(node, i, position)
-    if i[:position] == position
+  def position_offset(node, illustration)
+    if illustration[:position] == @inline_position
       "previous"
-    elsif i[:position] == position + 1 and @content.css("p").last == node
+    elsif illustration[:position] == @inline_position + 1 and (@content.css("p").last == node or node.next_element.name == "h1")
+      @inline_position += 1
       "next"
     else
       false
